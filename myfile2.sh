f0dadc56eac3b31779b4340c1e577d8c2792e59d
Mayank <maysinghal@icloud.com>
2023-01-25 15:03:50 +0530

    Second Commit

---
+++ myfile2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#### This script for automation
+
+echo "create directory"
+mkdir mayank-learning
+
+echo "Move to directory and create file there"
+cd mayank-learning
+touch learning-file
+
+
